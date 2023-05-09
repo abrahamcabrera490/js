70b8dd5508bf9f0d777a9071349a39c957d142ab
Marco Corona <a.corona@idbrics.com>
2023-05-09 07:05:19 -0600

    Update Main

--- main.js
+++ main.js
@@ -2,17 +2,30 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// Send static files
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  const html = `
+    <html>
+      <head>
+        <title>Home</title>
+      </head>
+      <body>
+        <h1>Home</h1>
+        <p>Esta es la página de inicio</p>
+      </body>
+    </html>
+  `;
+
+  res.send(html);
 });
 
-app.get("/hello/",(req,res)=>{
-
-res.send({saludo: "Bienvenido"});
-
+// Send welcome message
+app.get("/hello", (req, res) => {
+  // res.send({ saludo: "Bienvenido" });
+  res.json({ saludo: "Bienvenido" });
 });
 
-
+// Start server
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
