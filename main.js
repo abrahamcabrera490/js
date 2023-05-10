c2569e0aa2a101949ae823cb4804f6e64299d6c9
Marco Corona <a.corona@idbrics.com>
2023-05-10 12:58:21 -0600

    Update Tasks

--- main.js
+++ main.js
@@ -6,60 +6,26 @@ const starwars = require("./sw");
 
 app.use(bodyParser.json());
 
-// Send static files
-app.get("/", (req, res) => {
-  const titulo = "Hola Ing";
-  const html = `
-    <html>
-      <head>
-        <title>${titulo}</title>
-      </head>
-      <body>
-        <h1>${titulo}</h1>
-        <p>Esta es la página de inicio</p>
-      </body>
-    </html>
-  `;
-
-  res.send(html);
-});
-
 // Send welcome message
-app.get("/hello", (req, res) => {
-  // res.send({ saludo: "Bienvenido" });
-  res.json({ saludo: "Bienvenido" });
+app.get("/", (req, res) => {
+  res.json({ saludo: "App Works" });
 });
 
+/**
+ * CONECTAR SQLITE
+ *
+ * Ver imagen de referencia en carpeta sqlite
+ * Instalar sqlite3 https://www.npmjs.com/package/sqlite
+ * Genera un select y retornalo a la ruta "/sqlite/select"
+ *
+ * Extra: Genera un insert y retornalo a la ruta "/sqlite/insert"
+ * Extra: Genera un update y retornalo a la ruta "/sqlite/update"
+ * Extra: Genera un delete y retornalo a la ruta "/sqlite/delete"
+ */
 
-app.get("/starwars", async(req, res)=>{
-
-const datos = await starwars();
-const html = ` <table style ="border: 1px solid black">
-<tr>
-  <th>Nombre</th>
-  <th>Peso</th>
-  <th>Estatura</th>
-</tr>
-<tr>
-  <td>${datos.data.name}</td>
-  <td>${datos.data.mass}</td>
-  <td>${datos.data.height}</td>
-  
-</tr>
-
-</table> `
-res.send(html);
-});
-
-app.post("/hello", (req, res) => {
-  const { saludo } = req.body;
-
-  res.json( {saludo, name:"El pepe" } );
-});
-
-app.delete("/hello", (req, res) => {
-  res.json({ saludo: "Adiós" });
-});
+// app.get("/sqlite/select", (req, res) => {
+//   res.json(rows);
+// });
 
 // Start server
 app.listen(port, () => {
--- sw.js
+++ sw.js
@@ -1,10 +1,8 @@
-const axios = require('axios');
+const axios = require("axios");
 
-const starwars = async ()=>{
-    const resultado =  await axios.get('https://swapi.dev/api/people/1/');
-    return resultado;  
-   
-   }
+const starwars = async () => {
+  const resultado = await axios.get(`${process.env.STARWARS_APIURL}/people/1/`);
+  return resultado;
+};
 
-
-   module.exports = starwars;
+module.exports = starwars;
